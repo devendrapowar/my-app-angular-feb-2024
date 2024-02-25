9b3cf279dd5eff39a5fc17082e3f4103aef9cf21
Devendra <devendra.powar@rediffmail.com>
2024-02-25 20:44:23 +0530

    view encapsulation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,25 @@ import { ParentComponent } from './parent/parent.component';
 import { StudentComponent } from './student/student.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ShadowDomEncapsulationComponent } from './view-encapsulation/shadow-dom-encapsulation/shadow-dom-encapsulation.component';
+import { NoEncapsulationComponent } from './view-encapsulation/no-encapsulation/no-encapsulation.component';
+import { EmulatedEncapsulationComponent } from './view-encapsulation/emulated-encapsulation/emulated-encapsulation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,FooterComponent, ParentComponent, StudentComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    ParentComponent,
+    StudentComponent,
+    EmulatedEncapsulationComponent,
+    NoEncapsulationComponent,
+    ShadowDomEncapsulationComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'My App';
